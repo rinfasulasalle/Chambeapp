5da521b8163495651e7848fbf161614ae2a42f03
rinfasulasalle <rinfas@ulasalle.edu.pe>
2023-11-03 17:39:43 -0500

    arreglé conexion a la bd

--- backend/src/db.js
+++ backend/src/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose.connect(
-  "mongodb+srv://rinfas:hx3o4NjfaZ4ga0rj@cluster0.0vlghfl.mongodb.net/?retryWrites=true&w=majority",
+  "mongodb+srv://rinfas:hx3o4NjfaZ4ga0rj@cluster0.0vlghfl.mongodb.net/ChambeaBD?retryWrites=true&w=majority",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
