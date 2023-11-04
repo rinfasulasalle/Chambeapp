f8ae9991a17d59ef40073ebef294fa020506f62f
rinfasulasalle <rinfas@ulasalle.edu.pe>
2023-11-04 01:09:36 -0500

    falaba importar hahahaha

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const authenticateToken = require("./authenticateToken");
 const routes = require("./routes");
 
 const app = express();
